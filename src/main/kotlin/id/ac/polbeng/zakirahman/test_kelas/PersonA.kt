d9de328f8151a317e0ec67135363875fbf9398c3
ZakiRahman <mzakiriau@gmail.com>
2022-03-25 10:40:38 +0700

    mencoba konstruktor

---
+++ src/main/kotlin/id/ac/polbeng/zakirahman/test_kelas/PersonA.kt
@@ -0,0 +1,17 @@
+package id.ac.polbeng.zakirahman.test_kelas
+
+class PersonA (_firstName: String, _lastName: String, _age: Int) {
+    var firstName:String
+    var lastName:String
+    var age: Int
+    init {
+        firstName = _firstName
+        lastName = _lastName
+        age = _age
+    }
+}
+fun main(){
+    val budi = PersonA("Zaki", "Rahman", 21)
+    println("Name : ${budi.firstName} ${budi.lastName}")
+    println("Age : ${budi.age}")
+}
