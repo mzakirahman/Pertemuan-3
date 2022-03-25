a551fec384d6a046f143da5b5cd6e6c583bfbae1
ZakiRahman <mzakiriau@gmail.com>
2022-03-25 10:34:52 +0700

    membuat pemanggilan method greed

--- src/main/kotlin/id/ac/polbeng/zakirahman/test_kelas/GreeterTest.kt
+++ src/main/kotlin/id/ac/polbeng/zakirahman/test_kelas/GreeterTest.kt
@@ -3,4 +3,9 @@ package id.ac.polbeng.zakirahman.test_kelas
 fun main() {
     val greeter = Greeter()
     greeter.greet()
+    greeter.text = "Hi"
+    greeter.greet("zakirahman")
+    greeter.greet("Budi")
+    greeter.text = "Hello"
+    println(greeter.with_ret_val("Dono"))
 }
